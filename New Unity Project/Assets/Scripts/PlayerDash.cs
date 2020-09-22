46ad6da1a71865b0d58ea94a9073718192fd5e25
syntetizado <frankiss.pc@gmail.com>
2020-09-22 15:13:42 +0200

    player dash ( y script the raycast sin terminar)

---
+++ New Unity Project/Assets/Scripts/PlayerDash.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDash : MonoBehaviour
+{
+    //we take first rotation to be upwards
+    [Header("Variables iniciadas")]
+    public float rotacion_inicial;
+    public float dashDistance;
+    public KeyCode dashKey = KeyCode.Space;
+
+    //Use to apply force to RigidBody
+    Vector2 m_NewForce;
+
+    //Use to manipulate the RigidBody of a GameObject
+    Rigidbody2D m_Rigidbody;
+
+    //Use to manipulate the Transform of a GameObject
+    Transform m_Transform;
+
+    //positions
+    Vector2 prePos;
+    Vector2 postPos;
+    Vector2 forwardVector;
+
+    //vars
+    float rotationAngle;
+    float xImpulse;
+    float yImpulse;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       //Fetch the RigidBody component attached to the GameObject
+        m_Rigidbody = GetComponent<Rigidbody2D>();
+
+        //Fetch the Transform component attached to the GameObject
+        m_Transform = GetComponent<Transform>();
+
+        //initial vector
+        forwardVector = Vector2.up;
+
+        m_Rigidbody.drag = dashDistance*10;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        prePos = m_Transform.position;
+        if (prePos-postPos != Vector2.zero) {
+            forwardVector = prePos-postPos;
+            forwardVector.Normalize();
+        }
+
+        if (Input.GetKeyDown(dashKey))
+        {
+            //Use Impulse mode as a force on the RigidBody
+            m_Rigidbody.AddForce(forwardVector*100*dashDistance, ForceMode2D.Impulse);
+        }
+
+        postPos = m_Transform.position;
+    }
+}
---
+++ New Unity Project/Assets/Scripts/RaycastCollide.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastCollide : MonoBehaviour
+{
+    public GameObject objetivo;
+    public LayerMask Capa_de_collision;
+
+    RaycastHit2D hit;
+    Collider2D m_Collider;
+    Collider2D targetCollider;
+    Transform m_Transform;
+
+    //positions
+    Vector2 prePos;
+    Vector2 postPos;
+    Vector2 moveVector;
+    float test;
+
+    void Start()
+    {
+       //Fetch the RigidBody component attached to the GameObject
+        m_Collider = GetComponent<Collider2D>();
+
+        //Fetch the Transform component attached to the GameObject
+        m_Transform = GetComponent<Transform>();
+
+        //objective collider
+        targetCollider = objetivo.GetComponent<Collider2D>();
+
+        //initial vector
+        moveVector = Vector2.zero;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        prePos = m_Transform.position;
+
+        moveVector = postPos-prePos;
+
+        Debug.DrawRay(prePos, moveVector, Color.yellow);
+
+        RaycastHit2D[] hit = Physics2D.RaycastAll(prePos, postPos, moveVector.magnitude, Capa_de_collision);
+
+
+            foreach (RaycastHit2D element in hit)
+            {
+                Debug.Log(element.collider);
+            }
+
+
+        if (test < moveVector.magnitude) {
+            test = moveVector.magnitude;
+            Debug.Log(test);
+        }
+
+
+
+        /*if (GameObject.Find(hit.collider.gameObject.objetivo) ) {
+            Debug.Log("hit");
+        }*/
+
+        postPos = m_Transform.position;
+    }
+}
