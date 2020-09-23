9e8253226557f72c31e27bef63b65dcd96482429
syntetizado <frankiss.pc@gmail.com>
2020-09-23 15:48:48 +0200

    el jugador puede atacar

---
+++ New Unity Project/Assets/Scripts/Player/PlayerDirection.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDirection : MonoBehaviour
+{
+    public Camera camara_de_jugador;
+    [Header("Tiempo que tarda en mirar (segundos)")]
+    public float look_delay;
+
+    //positions
+    Vector2 pos;
+    Vector2 mousePos;
+    Vector2 lookDirection;
+    Transform m_Transform;
+
+    //rotation
+    Quaternion new_rotation;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Transform = GetComponent<Transform>();
+
+        //initial vector
+        lookDirection = Vector2.up;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        pos = m_Transform.position;
+        mousePos = camara_de_jugador.ScreenToWorldPoint(Input.mousePosition);
+        mousePos = new Vector2(mousePos.x, mousePos.y);
+
+        lookDirection = mousePos - pos;
+        if (lookDirection.magnitude>1) {
+
+            lookDirection.Normalize();
+            new_rotation = Quaternion.FromToRotation(Vector2.up, lookDirection);
+            m_Transform.rotation = Quaternion.Slerp(transform.rotation, new_rotation,  Time.deltaTime / look_delay);
+        }
+
+    }
+}
---
+++ New Unity Project/Assets/Scripts/Player/PlayerScythe.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScythe : MonoBehaviour
+{
+    [Header("Variables manipulables")]
+    public float attackSpeed;
+    public KeyCode attackKey;
+
+    [Header("Necesario configurar")]
+    public GameObject scythePivot;
+    public GameObject scytheTrail;
+
+    Animator anim;
+    TrailRenderer trail;
+    Animation slash;
+    bool can_attack;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = scythePivot.GetComponent<Animator>();
+        trail = scytheTrail.GetComponent<TrailRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (anim.GetCurrentAnimatorStateInfo(0).IsName("stop")) {
+            can_attack = true;
+            trail.Clear();
+        }
+
+        if (can_attack) {
+            if (Input.GetKeyDown(attackKey)) {
+                anim.Play("attack", 0, 1/attackSpeed);
+                can_attack = false;
+            }
+        }
+
+    }
+}
