a27df13e46ccd4f2aaada1f0dc4c1a73498012db
syntetizado <frankiss.pc@gmail.com>
2020-09-23 20:27:05 +0200

    merge

--- New Unity Project/Assets/Scripts/Player/PlayerTimeControl.cs
+++ New Unity Project/Assets/Scripts/Player/PlayerTimeControl.cs
@@ -7,12 +7,11 @@ public class PlayerTimeControl : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
